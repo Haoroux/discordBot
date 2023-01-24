935cfc93cb4f8a9e18a3022ca61da79a8a1bac81
Haoroux <52127278+Haoroux@users.noreply.github.com>
2023-01-24 19:34:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 import discord
 from discord import app_commands
 from discord.ext import commands
-from discord_slash import SlashCommand
 from dotenv import load_dotenv
 
 #importation du token du bot
@@ -11,7 +10,6 @@ load_dotenv(dotenv_path="config")
 
 #Démmarage et intents
 intents = discord.Intents.all()
-intents.members = True
 bot = commands.Bot(command_prefix='!',intents=intents)
 guild_ids = [1035614329678082098]
 
@@ -21,25 +19,39 @@ async def on_ready():
     synced = await bot.tree.sync()
     print("Slash CMDs Synced " + str(len(synced)))
 
-#Tout ce qui est slash commande
-@bot.tree.command(name="hello", description="shutting dow the bot")
-async def shutdown(interaction: discord.Interaction):
-    await interaction.response.send_message(content="that a test")
-    await bot.close()
+#Auto moderation
+IWords = ["fuck","pute","fdp","ip"]
 
+@bot.event
+async def on_message(message):
+    if message.content.lower() in IWords:
+        await message.channel.purge(limit=1)
+
+#pierre papier sciseaux
+@app_commands.choices(actions = [
+    app_commands.Choice(name="pierre", value="pierre"),
+    app_commands.Choice(name="papier", value="papier"),
+    app_commands.Choice(name="ciseaux", value="ciseaux")
+])
+
+@bot.tree.command(description="joue a pierre papier ciseaux avec moi")
+async def rps(interaction: discord.Interaction, actions: str):
+    if actions == 'pierre':
+        await interaction.response.send_message("you won")
+    elif actions == "papier":
+        await interaction.response.send_message("you lose")
+    else:
+        await interaction.response.send_message("you lose")
+
+
+#Tout ce qui est slash commande
+@bot.tree.command(name="test", description="That a test")
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message(content="that’s a test")
+
+#Lancement du programme
 bot.run(os.getenv("TOKEN"))
 
-#mots interdits + suppression
-#IWords = ["fuck","pute","fdp"]
-
-#@bot.event
-#async def on_message(message):
-#    if message.content.lower() in IWords:
-#        await message.channel.purge(limit=1)
-#        print('MessageDécidentMaitriser')
-
-#    if message.content.lower() == "chuchotePlusFort":
-#        await message.channel.send("j’aime et bien et je trouve sa beau")
 
 #quand une personne arrive sur le serv
 #@bot.event
